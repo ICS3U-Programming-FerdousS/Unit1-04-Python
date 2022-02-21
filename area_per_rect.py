eb5261a5c70fbaba8b7e875a15656bfd6e54662e
Ubuntu <ubuntu@ip-172-31-46-166.ec2.internal>
2022-02-21 05:53:33 +0000

    added code for basic math and rectAngle python

---
+++ area_per_rect.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python3
+# Created by: Ferdous Sediqi
+# Created on: Feb. 20, 2022
+# This program calculates and displays the area and perimeter of a
+# rectangle with length 5cm and width 3cm.
+
+
+def main():
+    print("For a rectangle that has dimensions")
+    print("length 5cm and width 7cm:")
+    print("")
+    print("Area = {} cm^2". format(5*7))
+    print("Perimeter = {} cm". format(2*(5+7)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Created by: Ferdous Sediqi
+# Created on: Feb. 19, 2022
+# This program does basic math.
+
+
+def main():
+    print("9 + 4 = {}". format(9+4))
+    print("7 - 2 = {}". format(7-2))
+    print("15 / 3 = {}". format(15/3))
+    print("8 - 1 / 2 = {}". format(8-1/2))
+    print("2^3 = {}". format(2**3))
+    print("1 / 3 = {}". format(1/3))
+
+
+if __name__ == "__main__":
+    main()
